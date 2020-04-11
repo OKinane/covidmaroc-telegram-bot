bd94773e52af4f157f1e794718f4d1f2b7ae5457
Kafil Nasdami <kafil.nasdami@gmail.com>
2020-04-11 18:43:47 +0100

    Using 'covid-ma' to fetch data

--- src/fetchCovidMarocData.js
+++ src/fetchCovidMarocData.js
@@ -1,21 +1,9 @@
-const { parseDate } = require("./lib/parseDate");
-const { JSDOM } = require("jsdom");
-const innerText = require("styleless-innertext");
+const getData = require("covid-ma");
 
 exports.fetchCovidMarocData = async () => {
-    const document = await JSDOM.fromURL('http://www.covidmaroc.ma/Pages/AccueilAR.aspx', {userAgent: 'AppleWebKit'});
-    const tbody = document.window.document.querySelector("#WebPartWPQ1 > div.ms-rtestate-field > div:nth-child(1) > table > tbody");
-    if (!tbody)
-        throw Error('Cannot find tbody html tag');
-    const text = innerText(tbody);
-    const noZeroWidthSpaces = text.replace(/\u{200b}/ug, '');
-    const singleWhiteSpaces = noZeroWidthSpaces.replace(/\s+/g, ' ').trim();
-    const [time, date, recovered, deaths, confirmed, negatives] = singleWhiteSpaces.split(' ');
-    return {
-        'date': parseDate(date, time),
-        'confirmed': parseInt(confirmed),
-        'deaths': parseInt(deaths),
-        'recovered': parseInt(recovered),
-        'negatives': parseInt(negatives)
-    };
-}
+  const result = await getData();
+  return {
+    date: result.lastUpdate,
+    ...result,
+  };
+};
--- src/lib/parseDate.js
+++
@@ -1,8 +0,0 @@
-var dayjs = require('dayjs');
-dayjs.extend(require('dayjs/plugin/customParseFormat'))
-
-const moroccoTimeOffset = "+0100";
-
-exports.parseDate = (date, time) => {
-    return dayjs(`${time} ${date} ${moroccoTimeOffset}`, 'HH[H]mm DD-MM-YYYY Z').toDate();
-}
