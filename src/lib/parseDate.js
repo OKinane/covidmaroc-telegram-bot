1b93eb6bf7a976b55a274625d1c375b197442f0d
OKinane <15254824+OKinane@users.noreply.github.com>
2020-06-06 17:29:42 +0100

    fix: update Morocco timezone

--- src/lib/parseDate.js
+++ src/lib/parseDate.js
@@ -1,7 +1,7 @@
 var dayjs = require('dayjs');
 dayjs.extend(require('dayjs/plugin/customParseFormat'))
 
-const moroccoTimeOffset = "+0000";
+const moroccoTimeOffset = "+0100";
 
 exports.parseDate = (date, time) => {
     return dayjs(`${time} ${date} ${moroccoTimeOffset}`, 'HH[H]mm DD-MM-YYYY Z').toDate();
